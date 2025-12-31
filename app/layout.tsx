5347417091587582c9b44e9c62cd01006d1aff36
Victor Malomo <malomovictor@gmail.com>
2025-12-31 17:40:29 +0100

    project set up - fonts, light rays and other styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Figtree, Martian_Mono } from "next/font/google";
+import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
-// import LightRays from
+import LightRays from "@/components/LightRays";
 
-const figTree = Figtree({
-  variable: "--font-figtree",
+const schibstedGrotesk = Schibsted_Grotesk({
+  variable: "--font-schibsted-grotesk",
   subsets: ["latin"],
 });
 
@@ -26,9 +26,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${figTree.variable} ${martianMono.variable} min-h-screen antialiased`}
+        className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-s antialiased`}
       >
-        {children}
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0.01}
+          />
+        </div>
+
+        <main>{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 const page = () => {
   return (
     <>
-      <h1>Welcome to Tailwind.css</h1>
+      <section className="flex flex-col h-[500px] justify-center">
+        <h1 className="text-center ">
+          Learn about every event <br /> Event you can't miss
+        </h1>
+        <p className="text-center mt-5">
+          Hackathons, Meetups and Conferences all in one place
+        </p>
+      </section>
     </>
   );
 };
